aca66cd1c4ec3a7165f0cb36695cce0507cbc9b8
YuqingHuang <yqnghang@gmail.com>
2021-04-26 18:16:57 -0400

    refactor:change comment in time_stats

--- bikeshare.py
+++ bikeshare.py
@@ -92,7 +92,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCalculating The Most Frequent Time of Travel...\n')
     start_time = time.time()
 
     # TO DO: display the most common month
